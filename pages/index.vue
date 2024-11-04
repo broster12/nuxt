2a0b94b697bb1f698bb737481d5d7de638dfa078
yusuke1816 <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2024-11-04 15:07:47 +0900

    addmap

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,53 @@
-<script setup lang="ts">
-definePageMeta({
-  layout: 'landing',
-  title: 'About use-bootstrap',
-  description: 'Quickly get a project started with any of our examples ranging from using parts of the framework to custom components and layouts.'
-})
-</script>
 <template>
-  <Container margin="t-5">
-    <HeroHeadingsDescription />
-  </Container>
-  あああ
-  <HeroLandingVb />
-  <Container margin="y-5">
-    <ColumnsOurService />
-  </Container>
-  <Background background-color="blue-100" pattern-src="/hero-patterns/jigsaw.svg" :pattern-parallax="0.05" margin="y-5">
-    <Container margin="y-5">
-      <ColumnsOurServiceHighlights />
-    </Container>
-  </Background>
-  <Container margin="y-5">
-    <BlocksKeyBenefits />
-  </Container>
-  <hr />
-  <Container margin="y-5">
-    <ColumnsServiceStats />
-  </Container>
-  <Background background-color="body-secondary" pattern-src="/hero-patterns/texture.svg" margin="y-5">
-    <Container margin="y-5">
-      <ColumnsSuccessStories />
-    </Container>
-  </Background>
-
+  <div class="container">
+    <h1 class="text-center">US Raper Map</h1>
+    <div class="row">
+      <div class="col-md-8">
+        <img src="/usmap.jpg" alt="US Map" class="img-fluid" />
+      </div>
+      <div class="col-md-4">
+        <img src="/juice.jpg" alt="Juice" class="juice-img" />
+      </div>
+      <div class="col-md-4">
+        <input type="range" min="0" max="data.length - 1" @input="updateLink" />
+        <span>Age </span>
+        <div>
+          <a :href="currentLink" target="_blank" class="btn btn-primary">詳細リンク</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const timeIndex = ref(0);
+const data = ref([]); // データの初期化
+const currentLink = ref(''); // 現在のリンク
+
+function updateLink() {
+  // スライダーのインデックスに基づいてリンクを更新
+  if (data.value[timeIndex.value]) {
+    currentLink.value = `https://example.com/details${timeIndex.value + 1}`; // リンクを更新
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+  height: auto;
+}
+
+.juice-img {
+  position: absolute; /* 絶対位置で配置 */
+  width: 20px; /* 小さくするためのサイズ調整 */
+  height: 20px; /* 高さを同じにして丸くする */
+  border-radius: 50%; /* 丸くするための設定 */
+  object-fit: cover; /* 画像が枠にフィットするように調整 */
+  left: 50%; /* 左から10% */
+  top: 50%; /* 上から10% */
+  transform: translate(-10%, -10%); /* 中心に配置するために移動 */
+}
+</style>
